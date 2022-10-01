2cf5b13d03592bfd4dc9e79288edef270be9dbc3
Genevieve Zenla <gen@nyble.dev>
2022-09-30 22:40:13 -0500

    Bullet hits

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,11 @@ const TURQUOISE: Color = Color::rgb(
 	0xaa as f32 / 256.0,
 	0x88 as f32 / 256.0,
 );
-
 const PURPLE: Color = Color::rgb(0.9, 0.8, 0.85);
+const MUR: u32 = 64;
 
 fn main() {
-	let smitty = Smitten::new((1280, 960), "Roundhead", 64);
+	let smitty = Smitten::new((1280, 960), "Roundhead", MUR);
 
 	let mut game = Game {
 		smitten: smitty,
@@ -79,6 +79,10 @@ struct Game {
 impl Game {
 	const BULLET_LIFESPAN: Duration = Duration::from_secs(1);
 	const BULLET_SPEED: f32 = 20.0;
+	const BULLET_WIDTH: u32 = 4;
+	const BULLET_WIDTH_MUR: f32 = Game::BULLET_WIDTH as f32 / MUR as f32;
+	const PLAYER_LENGTH: f32 = 0.5;
+	const PLAYER_DIM: Vec2 = Vec2::new(Game::PLAYER_LENGTH, Game::PLAYER_LENGTH);
 
 	pub fn rect<P: Into<Vec2>, D: Into<Vec2>, R: Into<Draw>>(&self, pos: P, dim: D, draw: R) {
 		self.smitten.rect(pos.into() - self.camera, dim, draw)
@@ -94,11 +98,11 @@ impl Game {
 		}
 
 		for enemy in &self.enemies {
-			self.rect(enemy.position, (0.5f32, 0.5f32), enemy.color)
+			self.rect(enemy.position, Game::PLAYER_DIM, enemy.color)
 		}
 
 		// Draw us. We're not affected by camera movement
-		self.smitten.rect((0f32, 0f32), (0.5f32, 0.5f32), TURQUOISE);
+		self.smitten.rect((0f32, 0f32), Game::PLAYER_DIM, TURQUOISE);
 	}
 
 	pub fn tick(&mut self) {
@@ -115,6 +119,8 @@ impl Game {
 		self.bullets
 			.iter_mut()
 			.for_each(|bul| bul.position += bul.velocity * dsec as f32);
+
+		self.do_hits();
 	}
 
 	pub fn shoot(&mut self) {
@@ -122,8 +128,48 @@ impl Game {
 		let bullet = Bullet::new(self.camera, direction * Game::BULLET_SPEED);
 		self.bullets.push(bullet);
 	}
+
+	fn do_hits(&mut self) -> Vec<(Enemy, Bullet)> {
+		let mut hit = vec![];
+
+		let mut enemies_alive = vec![];
+
+		'enemy: for enemy in self.enemies.drain(..) {
+			let mut unhit_bullets = vec![];
+
+			loop {
+				match self.bullets.pop() {
+					None => {
+						self.bullets.extend(unhit_bullets);
+						break;
+					}
+					Some(bullet) => {
+						if Self::enemy_hit(&enemy, &bullet) {
+							hit.push((enemy, bullet));
+							self.bullets.extend(unhit_bullets);
+							continue 'enemy;
+						} else {
+							unhit_bullets.push(bullet);
+						}
+					}
+				}
+			}
+
+			enemies_alive.push(enemy);
+		}
+
+		self.enemies = enemies_alive;
+
+		hit
+	}
+
+	// Lazy collisions; everything is a circle
+	fn enemy_hit(enemy: &Enemy, bullet: &Bullet) -> bool {
+		enemy.position.distance_with(bullet.position) < Game::PLAYER_LENGTH
+	}
 }
 
+#[derive(Clone, Debug, PartialEq)]
 struct Bullet {
 	position: Vec2,
 	velocity: Vec2,
@@ -140,6 +186,7 @@ impl Bullet {
 	}
 }
 
+#[derive(Clone, Debug, PartialEq)]
 struct Enemy {
 	position: Vec2,
 	color: Color,
